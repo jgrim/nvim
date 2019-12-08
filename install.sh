60ac5c1e8cfb2d9f7b1c2528d0adb9a712827221
Jason Grim <me@jasongrim.com>
2019-12-07 19:00:14 -0500

    Install script cleanup

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env sh
 
+# Install system packages
 BREW=$(which brew)
 APT_GET=$(which apt-get)
-
 if [[ ! -z "$BREW" ]]; then
-    $BREW install neovim composer global python python3 php@7.3 ripgrep
+    $BREW install git nvm neovim ctags global composer python python3 php@7.3 ripgrep
+fi
+if [[ ! -z "$APT_GET" ]]; then
+    $APT_GET #@todo add package list 
 fi
 
+# Install python for neovim
+pip2 install neovim --upgrade
+pip3 install neovim --upgrade
+
+# Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Install PHP Codeing Standards Fixer
+# Install PHP Coding Standards Fixer
 composer global require friendsofphp/php-cs-fixer
+
+# Install PHP Code Sniffer
+composer global require "squizlabs/php_codesniffer=*"
 
 nvim +'PlugInstall --sync' +qa
