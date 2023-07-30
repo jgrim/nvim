a9646ed6b30c67174aa3afb9a5c2ffa8c6f64a39
Jason Grim <jason@gr.im>
2023-07-30 09:18:42 -0400

    Updated nvim install on linux

--- install.sh
+++ install.sh
@@ -16,7 +16,14 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     APT=$(which apt)
     if [[ ! -z "$APT" ]]; then
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-        sudo $APT install git neovim universal-ctags global python3 python3-pip ripgrep
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
+            chmod u+x nvim.appimage && \
+            ./nvim.appimage --appimage-extract && \
+            ./squashfs-root/AppRun --version && \
+            sudo mv squashfs-root / && /
+            sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+        sudo $APT install git universal-ctags global python3 python3-pip ripgrep
     fi
 else
     echo "Unsupported OS"
